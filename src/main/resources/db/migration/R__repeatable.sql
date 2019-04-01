669de4a6e87e1f44ccbec1d9ec2984aba55c2fa5
DaniilMakarov <daniilmakarov00@mail.ru>
2019-04-02 01:20:01 +0700

    -

--- src/main/resources/db/migration/R__repeatable.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "Stock" (code,name,sale,buy)
-VALUES
-('TCS','TCS Group (Tinkoff)', 23, 22.7),
-('GAZP','ПAO Гaзпpoм', 125, 123.3);
--- src/main/resources/db/migration/V1__table_user.sql
+++ src/main/resources/db/migration/V1__table_user.sql
@@ -5,3 +5,25 @@ create table "User" (
     "salt"               varchar(128)  not null,
     check("login" ~ '^[a-z|A-Z|\d|_]')
 );
+CREATE TABLE "Stock" (
+	"id" serial  ,
+	"code" varchar NOT NULL,
+	"name" varchar NOT NULL,
+	"iconUrl" varchar DEFAULT 'icon.jpg',
+	"sale" numeric  NOT NULL,
+	"buy" numeric  NOT NULL,
+	CONSTRAINT Stock_pk PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Storage" (
+	"id" serial ,
+	"login" varchar NOT NULL,
+	"idStock" integer NOT NULL,
+	"count" integer NOT NULL,
+	CONSTRAINT Storage_pk PRIMARY KEY ("id")
+	);
+
+INSERT INTO "Stock" (code,name,sale,buy)
+VALUES
+('TCS','TCS Group (Tinkoff)', 23, 22.7),
+('GAZP','ПAO Гaзпpoм', 125, 123.3);
