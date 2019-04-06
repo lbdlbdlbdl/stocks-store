17e5ad5bd8ce8d443513cd09d04d38750d412abd
DaniilMakarov <daniilmakarov00@mail.ru>
2019-04-06 16:33:11 +0700

    реализовал history для транзакций,добавил запись историй при совершении транзакций

--- src/main/scala/ru/tinkoff/fintech/stocks/services/TransactionService.scala
+++ src/main/scala/ru/tinkoff/fintech/stocks/services/TransactionService.scala
@@ -1,23 +1,23 @@
 package ru.tinkoff.fintech.stocks.services
 
 import akka.actor.ActorSystem
-import ru.tinkoff.fintech.stocks.dao.{StockDao, StocksPackageDao, UserDao}
+import cats.instances.map
+import ru.tinkoff.fintech.stocks.dao.{StockDao, StocksPackageDao, TransactionHistoryDao, UserDao}
 import ru.tinkoff.fintech.stocks.db._
 import ru.tinkoff.fintech.stocks.http.Exceptions._
 import ru.tinkoff.fintech.stocks.http.JwtHelper
-import ru.tinkoff.fintech.stocks.http.Responses
+import ru.tinkoff.fintech.stocks.http.Responses.{HistoryTransaction, StockHistory}
 
 import scala.concurrent.{ExecutionContext, Future}
 import java.time.LocalDateTime
 
-import ru.tinkoff.fintech.stocks.http.Responses.HistoryTransaction
-
-case class Companion (user:User, packag:Option[StocksPackage], stock:Stock)
+case class Companion(user: User, packag: Option[StocksPackage], stock: Stock)
 
 class TransactionService(
                           val stocksPackageDao: StocksPackageDao,
                           val stockDao: StockDao,
-                          val userDao: UserDao)
+                          val userDao: UserDao,
+                          val transactionDao: TransactionHistoryDao)
                         (implicit val exctx: ExecutionContext,
                          implicit val system: ActorSystem) extends JwtHelper {
 
@@ -27,6 +27,7 @@ class TransactionService(
 
   //переделать с использованием Cats!!!
 
+  //достаем инфу о пользователе о его пакете на акцию и информацию о самой акции
   def companion(login: String, idStock: Long, amount: Int): Future[Companion] = {
     if (amount <= 0) throw ValidationException(s"Invalid value amount=$amount")
     for {
@@ -40,6 +41,8 @@ class TransactionService(
     } yield Companion(userInfo, package_, stock)
 
   }
+
+  def timeNow:String=LocalDateTime.now().toString
 
   def buyStock(login: String, idStock: Long, amount: Int): Future[Unit] = {
     for {
@@ -55,6 +58,8 @@ class TransactionService(
         case Some(value) => stocksPackageDao.updatePackage(idStock, value.count + amount)
         case None => stocksPackageDao.add(StocksPackage(None, companion.user.id.get, idStock, amount))
       }
+      history<- transactionDao.add(TransactionHistory
+      (None,login,idStock,amount,companion.stock.buyPrice * amount,timeNow,"buy"))
     } yield ()
 
   }
@@ -64,27 +69,33 @@ class TransactionService(
       companion <- companion(login: String, idStock: Long, amount: Int)
       removePackageStock <- companion.packag match {
         case Some(value) =>
-          if(amount>value.count) throw new Exception("Not enough shares in the account")
+          if (amount > value.count) throw new Exception("Not enough shares in the account")
           else stocksPackageDao.updatePackage(idStock, value.count - amount)
         case None => throw new Exception("Not enough shares in the account")
       }
       sellStock <- {
-        val newBalance = companion.user.balance + companion.stock.buyPrice * amount
+        val newBalance = companion.user.balance + companion.stock.salePrice * amount
         log.info(s"update user $login new balanace $newBalance")
         userDao.updateBalance(login, newBalance)
       }
+      history<- transactionDao.add(TransactionHistory
+      (None,login,idStock,amount,companion.stock.salePrice * amount,timeNow,"sell"))
 
     } yield ()
 
 
   }
+  def transformation(value:TransactionHistory): Future[HistoryTransaction] ={
+    for{
+      stock<-stockDao.getStock(value.idStock)
+      sample =StockHistory(stock.id,stock.code,stock.name)
+    }yield HistoryTransaction(sample,value.amount,value.totalPrice,value.date,value.`type`)
+  }
 
-  def history(login: String): Future[HistoryTransaction] = {
+  def history(login: String): Future[List[HistoryTransaction]] = {
     for {
-
-
-    } yield HistoryTransaction()
-
-
+      list <- transactionDao.find(login)
+      responses<-Future.sequence(list.map(transformation))
+    } yield responses
   }
 }
